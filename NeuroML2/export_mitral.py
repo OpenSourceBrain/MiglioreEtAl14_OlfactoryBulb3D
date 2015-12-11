52e80a8b4644918e1032b7671b733e9bb5cc643a
JustasB <justasb@users.noreply.github.com>
2015-12-10 21:06:14 -0700

    Builds the correct groups and works from NeuroML2 folder

--- NeuroML2/export_mitral.py
+++ NeuroML2/export_mitral.py
@@ -1,6 +1,3 @@
-import pydevd
-pydevd.settrace('10.211.55.3', port=4200, stdoutToServer=True, stderrToServer=True)
-
 import os
 import sys
 import glob
@@ -23,7 +20,7 @@ from neuroml import SegmentGroup
 
 if __name__ == "__main__":
 
-    num_cells_to_export = 2
+    num_cells_to_export = 30
 
     cells = []
     for mgid in range(num_cells_to_export):
@@ -44,6 +41,12 @@ if __name__ == "__main__":
         if os.path.isfile(newLoc):
             os.remove(newLoc)
         shutil.move(file, newLoc)
+
+    # Fix the network file to refer to correct cell files
+    with open (nml_net_file, "r") as netFile:
+        netTextFixed=netFile.read().replace('"../Mitral', '"Mitral')
+    with open(nml_net_file, "w") as netFile:
+        netFile.write(netTextFixed)
 
     for i in range(num_cells_to_export):
          
@@ -73,38 +76,54 @@ if __name__ == "__main__":
                     if memb.segments == bad_root:
                         memb.segments = root_id
 
-        # Delete all ModelViewParmSubset_N groups
+        largestGroup = None
+        # Delete all ModelViewParmSubset_N groups, saving the largest
         for g in xrange(len(cell.morphology.segment_groups)-1,-1,-1):  # Start from the end
             group = cell.morphology.segment_groups[g]
+
             if group.id.startswith('ModelViewParmSubset'):
                 cell.morphology.segment_groups.remove(group)
 
-        # Add the standard soma, dendrite, axon groups
-        somaGroup = SegmentGroup('GO:0043025', 'soma_group')
-        dendriteGroup = SegmentGroup('GO:0030425', 'dendrite_group')
-        axonGroup = SegmentGroup('GO:0030424', 'axon_group')
+                if largestGroup is None or len(largestGroup.includes) < len(group.includes):
+                    largestGroup = group
 
+        # Add the standard soma, dendrite, axon groups
+        somaGroup =     SegmentGroup('GO:0043025', 'soma_group')
+        dendriteGroup = SegmentGroup('GO:0030425', 'dendrite_group')
+        axonGroup =     SegmentGroup('GO:0030424', 'axon_group')
+
+        allGroup = None
         # Find the group with all segments
         for group in cell.morphology.segment_groups:
             if group.id == 'all':
                 allGroup = group
 
-        # Classify each include into a standard group
-        for include in allGroup.includes:
+        # If there is no "all" group, assume it's the largest of the ModelViewP... groups
+        if allGroup is None and largestGroup is not None:
+            allGroup = largestGroup
 
-            if include.segment_groups.startswith(('secden', 'priden', 'tuftden')):
-                dendriteGroup.includes.append(include)
+            # Create the 'all' group from the largest group
+            largestGroup.id = 'all'
+            cell.morphology.segment_groups.append(largestGroup)
 
-            elif include.segment_groups == 'soma':
-                somaGroup.includes.append(include)
+        if allGroup is not None:
 
-            elif include.segment_groups.startswith(('hillock', 'initialseg')):
-                axonGroup.includes.append(include)
+            # Classify each include of 'all' group into a standard group
+            for include in allGroup.includes:
 
-        # Attach the standard groups to the cell
-        cell.morphology.segment_groups.append(somaGroup)
-        cell.morphology.segment_groups.append(dendriteGroup)
-        cell.morphology.segment_groups.append(axonGroup)
+                if include.segment_groups.startswith(('secden', 'priden', 'tuftden')):
+                    dendriteGroup.includes.append(include)
+
+                elif include.segment_groups == 'soma':
+                    somaGroup.includes.append(include)
+
+                elif include.segment_groups.startswith(('hillock', 'initialseg')):
+                    axonGroup.includes.append(include)
+
+            # Attach the standard groups to the cell
+            cell.morphology.segment_groups.append(somaGroup)
+            cell.morphology.segment_groups.append(dendriteGroup)
+            cell.morphology.segment_groups.append(axonGroup)
 
         #Save the new NML
         pynml.write_neuroml2_file(nml_doc, nml_cell_file)
