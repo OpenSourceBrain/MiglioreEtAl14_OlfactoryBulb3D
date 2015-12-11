f5bf04ce9fc98b81278cc59d873f8e4154be9f07
JustasB <justasb@users.noreply.github.com>
2015-12-11 15:48:18 -0700

    Channel densities included in Mitral cells

--- NeuroML2/export_mitral.py
+++ NeuroML2/export_mitral.py
@@ -1,8 +1,6 @@
 import os
 import sys
-import glob
-import shutil
-import ntpath
+import neuroml
 
 #Nav to neuron folder where compiled MOD files are present
 os.chdir("../NEURON")
@@ -18,8 +16,7 @@ from pyneuroml.neuron import export_to_neuroml2
 from pyneuroml import pynml
 from neuroml import SegmentGroup
 
-if __name__ == "__main__":
-
+def __main__():
     num_cells_to_export = 30
 
     cells = []
@@ -61,58 +58,87 @@ if __name__ == "__main__":
                     if memb.segments == bad_root:
                         memb.segments = root_id
 
-        largestGroup = None
-        # Delete all ModelViewParmSubset_N groups, saving the largest
-        for g in xrange(len(cell.morphology.segment_groups)-1,-1,-1):  # Start from the end
-            group = cell.morphology.segment_groups[g]
+        # Replace ModelViewParmSubset_N groups with all, axon, soma, dendrite groups
+        buildStandardSegmentGroups(cell)
 
-            if group.id.startswith('ModelViewParmSubset'):
-                cell.morphology.segment_groups.remove(group)
+        # Add channel placeholders
+        nml_doc.includes.append(neuroml.IncludeType(href="channelIncludesPLACEHOLDER"))
+        cell.biophysical_properties = neuroml.BiophysicalProperties(id="biophysPLACEHOLDER")
 
-                if largestGroup is None or len(largestGroup.includes) < len(group.includes):
-                    largestGroup = group
-
-        # Add the standard soma, dendrite, axon groups
-        somaGroup =     SegmentGroup('GO:0043025', 'soma_group')
-        dendriteGroup = SegmentGroup('GO:0030425', 'dendrite_group')
-        axonGroup =     SegmentGroup('GO:0030424', 'axon_group')
-
-        allGroup = None
-        # Find the group with all segments
-        for group in cell.morphology.segment_groups:
-            if group.id == 'all':
-                allGroup = group
-
-        # If there is no "all" group, assume it's the largest of the ModelViewP... groups
-        if allGroup is None and largestGroup is not None:
-            allGroup = largestGroup
-
-            # Create the 'all' group from the largest group
-            largestGroup.id = 'all'
-            cell.morphology.segment_groups.append(largestGroup)
-
-        if allGroup is not None:
-
-            # Classify each include of 'all' group into a standard group
-            for include in allGroup.includes:
-
-                if include.segment_groups.startswith(('secden', 'priden', 'tuftden')):
-                    dendriteGroup.includes.append(include)
-
-                elif include.segment_groups == 'soma':
-                    somaGroup.includes.append(include)
-
-                elif include.segment_groups.startswith(('hillock', 'initialseg')):
-                    axonGroup.includes.append(include)
-
-            # Attach the standard groups to the cell
-            cell.morphology.segment_groups.append(somaGroup)
-            cell.morphology.segment_groups.append(dendriteGroup)
-            cell.morphology.segment_groups.append(axonGroup)
-
-        #Save the new NML
+        # Save the new NML
         pynml.write_neuroml2_file(nml_doc, nml_cell_file)
 
+        # Replace placeholders with contents from MitralCell...xml files
+        replaceChannelPlaceholders(nml_cell_file)
 
     pynml.nml2_to_svg(nml_net_file)
 
+def replaceChannelPlaceholders(nml_cell_file):
+
+    with open ("../NeuroML2/MitralCellBiophysicalProperties.xml", "r") as bioPhysFile:
+        bioPhysProps=bioPhysFile.read()
+
+    with open ("../NeuroML2/MitralCellChannelIncludes.xml", "r") as channelIncludesFile:
+        channelIncludes=channelIncludesFile.read()
+
+    with open (nml_cell_file, "r") as cellFile:
+        cellNMLreplaced=cellFile.read()\
+            .replace('<include href="channelIncludesPLACEHOLDER"></include>', channelIncludes)\
+            .replace('<biophysicalProperties id="biophysPLACEHOLDER"/>', bioPhysProps)\
+
+    with open(nml_cell_file, "w") as cellFile:
+        cellFile.write(cellNMLreplaced)
+
+def buildStandardSegmentGroups(cell):
+
+    largestGroup = None
+    # Delete all ModelViewParmSubset_N groups, saving the largest
+    for g in xrange(len(cell.morphology.segment_groups) - 1, -1, -1):  # Start from the end
+        group = cell.morphology.segment_groups[g]
+
+        if group.id.startswith('ModelViewParmSubset'):
+            cell.morphology.segment_groups.remove(group)
+
+            if largestGroup is None or len(largestGroup.includes) < len(group.includes):
+                largestGroup = group
+
+    # Add the standard soma, dendrite, axon groups
+    somaGroup = SegmentGroup('GO:0043025', 'soma_group')
+    dendriteGroup = SegmentGroup('GO:0030425', 'dendrite_group')
+    axonGroup = SegmentGroup('GO:0030424', 'axon_group')
+    allGroup = None
+
+    # Find the group with all segments
+    for group in cell.morphology.segment_groups:
+        if group.id == 'all':
+            allGroup = group
+
+    # If there is no "all" group, assume it's the largest of the ModelViewP... groups
+    if allGroup is None and largestGroup is not None:
+        allGroup = largestGroup
+
+        # Create the 'all' group from the largest group
+        largestGroup.id = 'all'
+        cell.morphology.segment_groups.append(largestGroup)
+
+    if allGroup is not None:
+
+        # Classify each include of 'all' group into a standard group
+        for include in allGroup.includes:
+
+            if include.segment_groups.startswith(('secden', 'priden', 'tuftden')):
+                dendriteGroup.includes.append(include)
+
+            elif include.segment_groups == 'soma':
+                somaGroup.includes.append(include)
+
+            elif include.segment_groups.startswith(('hillock', 'initialseg')):
+                axonGroup.includes.append(include)
+
+        # Attach the standard groups to the cell
+        cell.morphology.segment_groups.append(somaGroup)
+        cell.morphology.segment_groups.append(dendriteGroup)
+        cell.morphology.segment_groups.append(axonGroup)
+
+if __name__ == "__main__":
+    __main__()
