6954ad27107c883b94b0d5e93f7399863bbd7345
JustasB <justasb@users.noreply.github.com>
2015-12-21 10:24:10 -0700

    Parameterized network model launch

--- NEURON/customsim.py
+++ NEURON/customsim.py
@@ -1,8 +1,18 @@
+# Launch this with "python customsim.py 2 10"
+# to get a network with 2 Mitral cells and 10 Granule cells/Mitral (20 total)
 
-import custom_params
-custom_params.filename = 'fig7'
-import params
-import runsim
-runsim.build_complete_model('c10.dic')
-runsim.run()
+if __name__ == '__main__':
+
+    import sys
+    import custom_params
+    custom_params.filename = 'fig7'
+
+    if len(sys.argv) > 1:
+        custom_params.customMitralCount = int(sys.argv[1])
+        custom_params.customGranulesPerMitralCount = int(sys.argv[2])
+
+    import params
+    import runsim
+    runsim.build_complete_model('c10.dic')
+    runsim.run()
 
