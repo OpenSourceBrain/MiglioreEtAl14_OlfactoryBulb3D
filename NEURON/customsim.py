79c4617cc9d3f1c3353313256a468c4c99f9ee4e
JustasB <justasb@users.noreply.github.com>
2015-12-29 19:27:08 -0700

    Tweaks to running resized network

--- NEURON/customsim.py
+++ NEURON/customsim.py
@@ -3,16 +3,26 @@
 
 if __name__ == '__main__':
 
+def main():
     import sys
     import custom_params
     custom_params.filename = 'fig7'
 
+    offset = 0
+    if len(sys.argv) >= 2 and sys.argv[1] == "-python":
+        offset = 2
+
     if len(sys.argv) > 1:
-        custom_params.customMitralCount = int(sys.argv[1])
-        custom_params.customGranulesPerMitralCount = int(sys.argv[2])
+        mitralArg = int(sys.argv[1+offset])
+        granArg = int(sys.argv[2+offset])
+
+        custom_params.customMitralCount = mitralArg
+        custom_params.customGranulesPerMitralCount = granArg
 
     import params
     import runsim
     runsim.build_complete_model('c10.dic')
     runsim.run()
 
+if __name__ == '__main__':
+    main()
